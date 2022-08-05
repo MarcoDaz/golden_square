2bc9c97ff852532e12e1fff0bc5a05e25beaf9ba
MarcoDaz <80964100+MarcoDaz@users.noreply.github.com>
2022-08-05 17:54:59 +0100

    completed mock bite 1 challenge

--- 3_mocking_bites/1_unit_testing_parent_child_classes/challenge/spec/task_list_spec.rb
+++ 3_mocking_bites/1_unit_testing_parent_child_classes/challenge/spec/task_list_spec.rb
@@ -12,4 +12,30 @@ RSpec.describe TaskList do
   end
 
   # Unit test `#all` and `#all_complete?` behaviour
+  it "returns all tasks" do
+    task_list = TaskList.new
+    task_1 = double :task
+    task_2 = double :task
+    task_list.add(task_1)
+    task_list.add(task_2)
+    expect(task_list.all).to eq [task_1, task_2]
+  end
+
+  it "returns false if some tasks are incomplete" do
+    task_list = TaskList.new
+    task_1 = double :task_1, complete?: true
+    task_2 = double :task_2, complete?: true
+    task_list.add(task_1)
+    task_list.add(task_2)
+    expect(task_list.all_complete?).to eq true
+  end
+
+  it "returns false if some tasks are incomplete" do
+    task_list = TaskList.new
+    task_1 = double :task_1, complete?: true
+    task_2 = double :task_2, complete?: false
+    task_list.add(task_1)
+    task_list.add(task_2)
+    expect(task_list.all_complete?).to eq false
+  end
 end
